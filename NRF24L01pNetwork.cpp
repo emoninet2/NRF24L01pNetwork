a8443d46ec2a1b2fcf1780346021ebfad7a90abc
Emon <emon.1992@live.com>
2017-02-05 20:45:52 +0000

    update

---
+++ NRF24L01pNetwork.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   NRF24L01pNetwork.cpp
+ * Author: emon1
+ * 
+ * Created on February 5, 2017, 11:35 PM
+ */
+
+#include "NRF24L01pNetwork.h"
+
+NRF24L01pNetwork::NRF24L01pNetwork() {
+}
+
+NRF24L01pNetwork::NRF24L01pNetwork(const NRF24L01pNetwork& orig) {
+}
+
+NRF24L01pNetwork::~NRF24L01pNetwork() {
+}
+
+void NRF24L01pNetwork::initNetwork(uint16_t networkId, uint16_t nodeId){
+    ownNodeId = networkId;
+    ownNetworkId = nodeId;
+}
+void NRF24L01pNetwork::setAdjacentNode(pipe_t AssignedPipe, uint16_t adjNodeId, pipe_t AdjNodeRxPipe){
+    if((AssignedPipe>= PIPE_P1)&&(AssignedPipe <= PIPE_P5)){
+        AdjNode[AssignedPipe-1].nodeId = adjNodeId;
+        AdjNode[AssignedPipe-1].rxPipe = AdjNodeRxPipe;
+        AdjNode[AssignedPipe-1].status = 1;
+    }
+}
+
+void NRF24L01pNetwork::sendToAdjacent(networkPayload_t *NetPayload, adjacentNode_t *AdjNode){
+        Payload_t payload;
+        payload.address = ((uint64_t)ownNetworkId<<24) +( (uint64_t)(AdjNode->nodeId)<<8) + (uint64_t)(0xC0+AdjNode->rxPipe);
+        memcpy(payload.data, NetPayload, 32);
+        payload.length = 32;
+        TransmitPayload(&payload);
+        
+}
---
+++ NRF24L01pNetwork.h
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   NRF24L01pNetwork.h
+ * Author: emon1
+ *
+ * Created on February 5, 2017, 11:35 PM
+ */
+
+#ifndef NRF24L01PNETWORK_H
+#define NRF24L01PNETWORK_H
+
+#include "NRF24L01p/NRF24L01p.h"
+#include "NRF24L01pNetworkConfig.h"
+
+class NRF24L01pNetwork : public NRF24L01p{
+public:
+    NRF24L01pNetwork();
+    NRF24L01pNetwork(const NRF24L01pNetwork& orig);
+    virtual ~NRF24L01pNetwork();
+    
+    typedef struct {
+        uint16_t srcNodeId;
+        uint16_t destNodeId;
+        uint8_t pid;
+        uint8_t packetInfo;
+        uint8_t *payload;
+    }networkPayload_t;
+    
+    typedef struct{
+        uint16_t nodeId;
+        pipe_t rxPipe;
+        uint8_t status;
+    }adjacentNode_t;
+    
+    typedef struct{
+        uint16_t destNodeId;
+        adjacentNode_t viaAdjNode;
+    }routingNode_t;
+    
+    uint16_t ownNodeId;
+    uint16_t ownNetworkId;
+    adjacentNode_t  AdjNode[5];
+    routingNode_t     RoutingTable[20];
+    
+    void initNetwork(uint16_t networkId, uint16_t nodeId);
+    void setAdjacentNode(pipe_t AssignedPipe, uint16_t adjNodeId, pipe_t AdjNodeRxPipe);
+    void sendToAdjacent(networkPayload_t *NetPayload, adjacentNode_t *AdjNode);
+    
+private:
+
+};
+
+#endif /* NRF24L01PNETWORK_H */
+
